37ff68c9129f1f1652320e51678b4261ab163016
Clo91eaf <1303076137@qq.com>
2021-11-18 16:23:11 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+
+import { Button, Select,Container, Aside, Header, Main, Menu, MenuItem, MenuItemGroup, Submenu, Dropdown, DropdownItem, DropdownMenu, Row, Col, Card} from 'element-ui';
+Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Submenu)
+Vue.use(Dropdown)
+Vue.use(DropdownItem)
+Vue.use(DropdownMenu)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
+
+// Vue.use(ElementUI);
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userImg" alt="">
+          <div class="userInfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登陆时间：<span>2021-7-19</span></p>
+          <p>上次登陆地点：<span>武汉</span></p>
+        </div>
+      </el-card>
+    </el-col>
+    <el-col></el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      userImg: require("../../assets/images/user.png")
+    }
+  }
+}
+</script>
